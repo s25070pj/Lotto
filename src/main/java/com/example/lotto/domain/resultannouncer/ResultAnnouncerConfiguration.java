1db01099ed4df813816927b32f6bbb9747b94cc8
brash <s25070@pjwstk.edu.pl>
2024-03-08 11:31:32 +0100

    created configuration for unit tests

--- src/main/java/com/example/lotto/domain/resultannouncer/ResultAnnouncerConfiguration.java
+++ src/main/java/com/example/lotto/domain/resultannouncer/ResultAnnouncerConfiguration.java
@@ -1,4 +1,12 @@
 package com.example.lotto.domain.resultannouncer;
 
+import com.example.lotto.domain.resultchecker.ResultCheckerFacade;
+
+import java.time.Clock;
+
 public class ResultAnnouncerConfiguration {
+
+    ResultAnnouncerFacade createForTest(ResultCheckerFacade resultCheckerFacade, ResponseRepository responseRepository, Clock clock){
+        return new ResultAnnouncerFacade(resultCheckerFacade, responseRepository, clock);
+    }
 }
