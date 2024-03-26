c537b6fb1285dc18b76647bad7f0277e2eeb0a49
brash <s25070@pjwstk.edu.pl>
2024-03-26 17:25:22 +0100

    refactor

--- src/integration/java/pl/lotto/BaseIntegrationTest.java
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -1,9 +1,11 @@
 package pl.lotto;
 
 import com.example.lotto.LottoApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.utility.DockerImageName;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
@@ -22,11 +23,14 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 @ActiveProfiles("integration")
 @AutoConfigureMockMvc
 @Testcontainers
-@AllArgsConstructor
+
 public class BaseIntegrationTest{
 
-    private final MockMvc mockMvc;
-    private final ObjectMapper objectMapper;
+    @Autowired
+    public MockMvc mockMvc;
+    @Autowired
+    public ObjectMapper objectMapper;
+
 
     private static final String WIRE_MOCK_HOST = "http://localhost";
 
@@ -44,6 +48,8 @@ public class BaseIntegrationTest{
 //        registry.add("offer.http.client.config.uri", () -> WIRE_MOCK_HOST);
 //        registry.add("offer.http.client.config.port", () -> wireMockServer.getPort());
     }
+
+
 
 
 }
