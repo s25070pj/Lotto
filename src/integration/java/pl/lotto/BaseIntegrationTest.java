2adf5ef3b8546c811f4fe649828f896f6d885241
brash <s25070@pjwstk.edu.pl>
2024-03-26 11:48:33 +0100

    preparing environment to integration tests

---
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -0,0 +1,8 @@
+package pl.lotto;
+
+import com.example.lotto.LottoApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LottoApplication.class)
+public class BaseIntegrationTest    {
+}
--- src/test/java/com/example/lotto/domain/numbergenerator/NumberGeneratorFacadeTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.lotto.domain.numbergenerator;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class NumberGeneratorFacadeTest {
-
-}
